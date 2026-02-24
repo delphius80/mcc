24574fe4238b9a6b1c7c1003b8269ca392166801
delphius80 <delphius80@gmail.com>
2026-02-25 00:08:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,21 @@
 import time
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    ChatJoinRequestHandler,
+    MessageHandler,
+    CallbackQueryHandler,
+    filters,
+)
+import os
 
 print("Bot stub started")
 
 while True:
     time.sleep(60)
+
